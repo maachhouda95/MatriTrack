33f4e84f42cf05d88c61b5a0658c138b93fb4a67
maachhouda95 <maachhouda95@gmail.com>
2026-08-12 01:29:29 +0100

    connect flask to mysql

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-from flask import flask
+from flask import Flask
+import mysql.connector
+
+app = Flask(__name__)
+
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="houda2004",
+    database="MatriTrack"
+)
+
+print("Database connected successfully!")
+
+
+@app.route("/")
+def home():
+    return "Flask is working!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
